f7106fc0b643da4e6d168a5212021c48d5297bb7
mark <markjava@foxmail.com>
2020-12-27 22:24:07 +0800

    对京东的封装

--- handler/goods.py
+++ handler/goods.py
@@ -15,6 +15,18 @@ class GoodsHandler(tornado.web.RequestHandler):
         page_size = self.get_query_argument('size')
         goods_type = self.get_query_argument('type')
         logging.info('query params, page_no:%s,page_size:%s,goods_type:%s', page_no, page_size, goods_type)
+        try:
+            c = GoodsEnum(str(goods_type))
+        except ValueError:
+            c = 0
+                 
+        if c == GoodsEnum.JD:
+            info = self.get_jd()
+        elif c == GoodsEnum.PDD:
+            print('pdd')
+        self.write_json(info)
+
+    def get_jd(self):
         conf = self.conf.get('jingdong')
         
         if not conf.get('is_open'):
@@ -30,7 +42,7 @@ class GoodsHandler(tornado.web.RequestHandler):
         kwargs={'group_material_id': group_material_id,
                 'app_key': app_key, 'secret_key': app_secret, 'site_id': site_id, 'suo_mi_token': suo_im,'page_no':page_no, 'page_size': page_size}
         info=jingfen_query(**kwargs)
-        self.write_json(info)
+        return info
 
     def write_json(self, info):
         self.set_header('Content-Type', 'application/json')
